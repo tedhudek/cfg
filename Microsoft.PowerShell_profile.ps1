22e90ebf0fdfb4635e7fd7d71aec5d547edbcb35
Ted Hudek <tedhudek@gmail.com>
2016-08-10 11:50:16 -0700

    add PS profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,12 @@
+﻿
+# Load posh-git example profile
+. 'C:\repo\github\posh-git\profile.example.ps1'
+
+$global:GitPromptSettings.WorkingForegroundColor    = [ConsoleColor]::Yellow 
+$global:GitPromptSettings.LocalWorkingStatusForegroundColor  = [ConsoleColor]::Yellow
+$global:GitPromptSettings.WorkingForegroundBrightColor  = [ConsoleColor]::Yellow
+$global:GitPromptSettings.BeforeStashForegroundColor  = [ConsoleColor]::Yellow
+$global:GitPromptSettings.LocalWorkingStatusForegroundBrightColor  = [ConsoleColor]::Yellow
+$global:GitPromptSettings.BranchBehindStatusForegroundColor  = [ConsoleColor]::Yellow
+$global:GitPromptSettings.AfterStashForegroundColor  = [ConsoleColor]::Yellow
+$global:GitPromptSettings.StashForegroundColor  = [ConsoleColor]::Yellow
